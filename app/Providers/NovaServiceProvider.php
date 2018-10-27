aab620de5450d8e221a62f865f34215b97356be1
Tomasz Lotocki <tom@gazeta.ie>
2018-10-27 21:44:43 +0100

    gate for access to alec and tom

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -43,7 +43,8 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     {
         Gate::define('viewNova', function ($user) {
             return in_array($user->email, [
-                //
+                'tomcentrumpl@gmail.com',
+                'alec@citltd.ie'
             ]);
         });
     }
