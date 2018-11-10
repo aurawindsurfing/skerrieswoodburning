f0e9f95e4c7a6593c7af108fb3b6ed83e77e6761
Tomasz Lotocki <tom@gazeta.ie>
2018-11-10 23:45:02 +0000

    nexmo sms

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -5,15 +5,19 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
 use App\Booking;
+use Nexmo\Laravel\Facade\Nexmo;
+
 
 class TestController extends Controller
 {
     public function test()
     {
-    
-        $b = Booking::find(85);
 
-        dump($b->course->course_type->default_rate);
+        Nexmo::message()->send([
+            'to'   => '353862194744',
+            'from' => 'medlab',
+            'text' => 'test',
+        ]);
 
     }
 }
