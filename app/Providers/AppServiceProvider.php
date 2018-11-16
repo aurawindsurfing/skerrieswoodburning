dff9281870d372f61d6cbd82e5be87262be3516e
Tomasz Lotocki <tom@gazeta.ie>
2018-11-16 13:49:26 +0000

    telescope in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,8 +23,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if ($this->app->isLocal()) {
+        // if ($this->app->isLocal()) {
             $this->app->register(TelescopeServiceProvider::class);
-        }
+        // }
     }
 }
