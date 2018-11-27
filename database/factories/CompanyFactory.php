44630d4721d82e64b3c2346e61c5f0a50e442c29
Tomasz Lotocki <tom@gazeta.ie>
2018-11-27 17:04:04 +0000

    removed fake emails and phonenumbers for testing

--- database/factories/CompanyFactory.php
+++ database/factories/CompanyFactory.php
@@ -6,8 +6,8 @@ $factory->define(App\Company::class, function (Faker $faker) {
     return [
         'name' => $faker->company,
         'address' => $faker->streetAddress,
-        'phone' => $faker->e164PhoneNumber,
-        'email' => $faker->safeEmail,
+        'phone' => $faker->randomElement(['0862194744','0868065966']),
+        'email' => $faker->randomElement(['tomcentrumpl@gmail.com', 'alec@citltd.ie', 'hank@citltd.ie']),
         'tax' => $faker->randomNumber($nbDigits = 7, $strict = true) . $faker->randomElement(['Q','W','E','R','T','Y']),
         'payment_method' => $faker->randomElement(['CC','EFT','Cash','Cheque']),
     ];
--- database/factories/ContactFactory.php
+++ database/factories/ContactFactory.php
@@ -5,8 +5,8 @@ use Faker\Generator as Faker;
 $factory->define(App\Contact::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
-        'phone' => $faker->e164PhoneNumber,
-        'email' => $faker->safeEmail,
+        'phone' => $faker->randomElement(['0862194744','0868065966']),
+        'email' => $faker->randomElement(['tomcentrumpl@gmail.com', 'alec@citltd.ie', 'hank@citltd.ie']),
         'accounts_payable' => $faker->randomElement([true,false]),
         'company_id' => factory('App\Company')->create()->id,
     ];
