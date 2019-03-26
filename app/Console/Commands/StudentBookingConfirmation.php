3672c1ee0fc0546a88bcd2d24a5919cdf9f2d41c
Tomasz Lotocki <tom@gazeta.ie>
2019-03-26 11:57:43 +0000

    fixes missing Carbon use in StudentBookingConfirmation

--- app/Console/Commands/StudentBookingConfirmation.php
+++ app/Console/Commands/StudentBookingConfirmation.php
@@ -6,6 +6,7 @@ use App\Booking;
 use App\Notifications\StudentConfirmation;
 use Illuminate\Console\Command;
 use Propaganistas\LaravelPhone\PhoneNumber;
+use Carbon\Carbon;
 
 class StudentBookingConfirmation extends Command
 {
