15074e83562e5a276f6566f3fd062a1b04432794
Tomasz Lotocki <tom@gazeta.ie>
2019-03-26 14:49:35 +0000

    changes footer

---
+++ resources/views/vendor/nova/partials/footer.blade.php
@@ -0,0 +1,7 @@
+<p class="mt-8 text-center text-xs text-80">
+    <a href="http://tomaszlotocki.com" class="text-primary dim no-underline">tomaszlotocki.com</a>
+    <span class="px-1">&middot;</span>
+    &copy; {{ date('Y') }} Tomasz Lotocki
+    <span class="px-1">&middot;</span>
+    {{-- v{{ Laravel\Nova\Nova::version() }} --}}
+</p>
