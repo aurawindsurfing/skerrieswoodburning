9e47342a9a97fb9e06a7f0659d52659ce3f1df20
Tomasz Lotocki <tom@gazeta.ie>
2020-02-26 14:22:17 +0000

    adds tailwind

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+require('laravel-mix-purgecss');
 
 /*
  |--------------------------------------------------------------------------
@@ -12,4 +13,9 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+    .postCss('resources/css/app.css', 'public/css/app.css', [
+        require('tailwindcss')('tailwind.config.js')
+    ])
+    .purgeCss()
+    .browserSync({ proxy: 'cit.test', notify: false})
+    .version();
