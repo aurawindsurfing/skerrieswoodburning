a58ca55ce68373bf406e2a87e7b0555648ec6546
Tomasz Lotocki <tom@gazeta.ie>
2020-02-28 01:06:47 +0000

    table colours

--- resources/views/sections/courses-list.blade.php
+++ resources/views/sections/courses-list.blade.php
@@ -26,7 +26,7 @@
                                 <div class="min-w-0 flex-1 px-4 md:grid md:grid-cols-2 md:gap-4">
                                     <div>
                                         <div
-                                            class="text-sm leading-5 font-medium text-indigo-600 truncate">{{$course->course_type->name}}</div>
+                                            class="text-base leading-5 font-medium text-blue-600 truncate">{{$course->course_type->name}}</div>
                                         <div class="mt-2 flex items-center text-sm leading-5 text-gray-500">
                                             {{--                                            <svg class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" fill="currentColor" viewBox="0 0 20 20">--}}
                                             {{--                                                <path fill-rule="evenodd" d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884zM18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" clip-rule="evenodd"/>--}}
