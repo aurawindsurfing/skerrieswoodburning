3aff14e353838e563d5e164f84130cec399b51ea
Tomasz Lotocki <tom@gazeta.ie>
2020-02-28 18:38:27 +0000

    logos magic

--- resources/views/sections/logos.blade.php
+++ resources/views/sections/logos.blade.php
@@ -1,21 +1,16 @@
 <div class="bg-white">
-    <div class="max-w-screen-xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
-        <div class="grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-5">
-            <div class="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
-                <img class="h-12" src="/images/logos/tuple-logo.svg" alt="Tuple" />
-            </div>
-            <div class="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
-                <img class="h-12" src="/images/logos/mirage-logo.svg" alt="Mirage" />
-            </div>
-            <div class="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
-                <img class="h-12" src="/images/logos/statickit-logo.svg" alt="StaticKit" />
-            </div>
-            <div class="col-span-1 flex justify-center md:col-span-3 lg:col-span-1">
-                <img class="h-12" src="/images/logos/transistor-logo.svg" alt="Transistor" />
-            </div>
-            <div class="col-span-2 flex justify-center md:col-span-3 lg:col-span-1">
-                <img class="h-12" src="/images/logos/workcation-logo.svg" alt="Workcation" />
-            </div>
+    <div class="max-w-screen-xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
+        <p class="text-center text-base leading-6 font-semibold uppercase text-gray-600 tracking-wider">
+            Trusted by leading Irish companies
+        </p>
+        <div class="mt-6 grid grid-cols-2 gap-0.5 md:grid-cols-3 lg:mt-8">
+            @foreach($logos as $logo)
+                <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+                    <img src="{{ $logo }}"/>
+                    {{--                    <img src="{{ Cloudder::secureShow('cit/'.$logo, config('settings.cloudinary_logo')) }}" />--}}
+                    {{--                    <img class="w-full" src="{{ Cloudder::secureShow('cit/pictures/digger-safety', config('settings.cloudinary_logo')) }}" alt="Digger Safety" />--}}
+                </div>
+            @endforeach
         </div>
     </div>
 </div>
