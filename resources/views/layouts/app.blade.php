fb57526ecb9558cd801387de7d9f76856e4b77d9
Tomasz Lotocki <tom@gazeta.ie>
2020-02-29 11:15:09 +0000

    wip

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,26 +12,27 @@
     <meta name='description' itemprop='description' content="@yield('description')" />
     <meta name='keywords' content="@yield('keywords')" />
     <meta name="robots" content="index, follow" />
-    <meta property="og:description"content="@yield('description')" />
-    <meta property="og:title"content="{{ env('APP_NAME') }} - @yield('title')" />
-    <meta property="og:url"content="{{ url()->current() }}" />
-    <meta property="og:type"content="article" />
+    <meta property="og:description" content="@yield('description')"/>
+    <meta property="og:title" content="{{ env('APP_NAME') }} - @yield('title')"/>
+    <meta property="og:url" content="{{ url()->current() }}"/>
+    <meta property="og:type" content="article"/>
 
-    <meta property="og:locale"content="{{ app()->getLocale() }}" />
-{{--    @foreach(localization()->getSupportedLocales() as $key => $locale)--}}
-{{--        <meta property="og:locale:alternate"content="{{ $key }}" />--}}
-{{--    @endforeach--}}
-    <meta property="og:site_name"content="{{ env('APP_NAME') }}" />
-    <meta property="og:image:url"content="{{ Cloudder::secureShow('/cit/hero', config('settings.cloudinary_optimised_jpg')) }}" />
-    <meta property="og:image:size"content="300" />
+    <meta property="og:locale" content="{{ app()->getLocale() }}"/>
+    {{--    @foreach(localization()->getSupportedLocales() as $key => $locale)--}}
+    {{--        <meta property="og:locale:alternate"content="{{ $key }}" />--}}
+    {{--    @endforeach--}}
+    <meta property="og:site_name" content="{{ env('APP_NAME') }}"/>
+    <meta property="og:image:url"
+          content="{{ Cloudder::secureShow('/cit/pictures/manual-handling', config('settings.cloudinary_optimised_jpg')) }}"/>
+    <meta property="og:image:size" content="300"/>
 
-    <meta name="twitter:card"content="@yield('description')" />
-    <meta name="twitter:title"content="{{ env('APP_NAME') }} - @yield('title')" />
-{{--    <meta name="twitter:site"content="@MedicusMedical" />--}}
+    <meta name="twitter:card" content="@yield('description')"/>
+    <meta name="twitter:title" content="{{ env('APP_NAME') }} - @yield('title')"/>
+    {{--    <meta name="twitter:site"content="@MedicusMedical" />--}}
 
-{{--    @foreach(localization()->getSupportedLocales() as $key => $locale)--}}
-{{--        <link rel="alternate" hreflang="{{ $key }}" href="{{ localization()->getLocalizedURL($key) }}">--}}
-{{--    @endforeach--}}
+    {{--    @foreach(localization()->getSupportedLocales() as $key => $locale)--}}
+    {{--        <link rel="alternate" hreflang="{{ $key }}" href="{{ localization()->getLocalizedURL($key) }}">--}}
+    {{--    @endforeach--}}
 
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
@@ -44,8 +45,6 @@
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
 
     <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
-
-{{--    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.0.1/dist/alpine.js" defer></script>--}}
 
     {{--    @include('partials.crisp')--}}
 
