5bececd7ed94e9d315bb35e456188774493427cc
Tomasz Lotocki <tom@gazeta.ie>
2020-11-28 16:20:53 +0000

    fixes logos flow

--- resources/views/sections/logos.blade.php
+++ resources/views/sections/logos.blade.php
@@ -1,10 +1,10 @@
 <div class="bg-white">
-    <div class="max-w-screen-xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
+    <div class="max-w-5xl xl:max-w-6xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
         <p class="text-center text-base leading-6 font-semibold uppercase text-gray-600 tracking-wider">
             Trusted by leading Irish companies
         </p>
-        <div class="grid grid-cols-4 md:grid-cols-7 mt-6 lg:mt-8">
-            @foreach($logos as $logo)
+        <div class="flex flex-wrap mt-6 lg:mt-8">
+        @foreach($logos as $logo)
                 <div class="justify-center m-auto py-2 px-2">
                     <img src="{{ $logo }}"/>
                 </div>
