82c91f9665764a8f8661f1bc84cc8ff0fd342011
aurawindsurfing <tom@gazeta.ie>
2023-12-08 11:16:39 +0000

    wip 3

--- resources/views/sections/aboutus.blade.php
+++ resources/views/sections/aboutus.blade.php
@@ -5,13 +5,24 @@
                 <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">About Us
                 </h2>
                 <div class="pt-8">
-                    <div class="relative flex pb-8 pt-80">
+                    <div class="relative flex-col pt-80">
                         <a href="{{'/images/skerries-news.pdf'}}">
-                            <img src="{{'/images/skerries-news.png'}}" alt="" class="absolute inset-0 w-full mx-auto">
+                            <img src="{{'/images/skerries-news.png'}}" alt="" class="absolute inset-0 -z-10 h-full w-full object-cover">
                         </a>
                     </div>
                 </div>
-                <p class="mt-2 text-lg leading-8 text-gray-600">
+                <p class=" text-lg leading-8 text-gray-600">
+                    The wood-burned coaster school project is an engaging and creative exploration that allows students to delve into the ancient art of pyrography, turning simple wooden coasters into personalized works of functional art. Under the guidance of their art teacher, students embark on a journey that combines artistic expression with the skillful use of wood-burning tools.
+
+                    The project begins with a design phase, encouraging students to unleash their creativity and conceptualize unique patterns, images, or themes for their coasters. Whether drawing inspiration from nature, personal experiences, or abstract concepts, each student is encouraged to develop a design that speaks to their individual style and vision.
+
+                    Armed with wood-burning tools, students carefully and deliberately apply controlled heat to the wooden surface, bringing their designs to life through the art of pyrography. The process demands precision, as students navigate the tools to create intricate details and varying degrees of shading. This hands-on experience not only enhances their artistic skills but also provides a lesson in patience and craftsmanship.
+
+                    The completed wood-burned coasters showcase a diverse range of designs, from elaborate and detailed patterns to more minimalist and abstract compositions. The natural grain of the wood interacts with the burned designs, adding depth and character to each piece.
+
+                    Beyond the artistic aspect, the project instills an appreciation for the properties of wood and the transformative nature of the pyrography process. As the students witness their designs take shape on the coasters, a sense of accomplishment and pride permeates the classroom.
+
+                    Ultimately, the wood-burned coaster school project not only serves as a platform for artistic expression but also introduces students to the intersection of art and craftsmanship. The finished coasters become functional art pieces, ready to be used and cherished, representing the unique creativity and skill of each student.
                 </p>
             </div>
         </div>
